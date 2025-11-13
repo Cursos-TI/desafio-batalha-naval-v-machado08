6ef9012a39310af63cc48c7ff1379b1db1fb3550
v-machado08 <vitinmachadu@gmail.com>
2025-11-13 13:09:49 +0000

    gerei o tabuleiro com a linha de 0 a 9 e a coluna de A a J. Tambem comentei o codigo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
 
-int main (){
+int main() {
+    //array linhas (A a J)
+    char linhas[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int tabuleiro[10][10];
 
-    char linha [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',};
-    int tabuleiro [10][10];
- 
 
+    //tabuleiro inicial com 0s
     for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++){
-        tabuleiro[i][j] = 0;
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
         }
-
     }
 
-      for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++){
-          printf("%d ", tabuleiro [i] [j] );
-          
+
+    //imprime a coluna (0 a 9)
+    printf("   "); //esse espaço eu utilizei para ficarem alinhadas as colunas
+    for (int j = 0; j < 10; j++) {
+        printf("%d ", j);
+    }
+    printf("\n"); //pula para uma nova linha depois da sequencia 0 a 9
+
+
+
+    //imprime A,B,C,D,E....
+    for (int i = 0; i < 10; i++) {
+        printf("%c  ", linhas[i]); 
+
+        
+        // Imprime 10 zeros para alinha atual 
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
         }
-      printf("\n");
-      }
-  
+        printf("\n"); 
+    }
 
- return 0;
-
+    return 0;
 }
-
