09b5f123e0dc15d2e0b7158c6e5722e95dfe8962
v-machado08 <vitinmachadu@gmail.com>
2025-11-13 23:12:50 +0000

    Adcionei os navios nas diagonais (principal e secundaria), deixei comentado. Findado o nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,7 +6,7 @@ int main() {
     int tabuleiro[10][10];
 
 
-    //tabuleiro inicial com 0s
+    //tabuleiro inicial com 0s representando a agua
     for (int i = 0; i < 10; i++) { 
         for (int j = 0; j < 10; j++) {
             tabuleiro[i][j] = 0;
@@ -15,7 +15,7 @@ int main() {
 
 
     //POSICIONAMENTO DOS NAVIOS (Navio 1) 3 posiçoes na horizontal
-    //Começa na Linha 'B', coluna 1
+    //Começa na Linha 'B', coluna 3
     int navio1_linha_inicio = 1; 
     int navio1_coluna_inicio = 3; 
 
@@ -32,6 +32,18 @@ int main() {
 
     for (int k = 0; k < 3; k++) {//3 posições uma embaixo da outra com o número 3
         tabuleiro[navio2_linha_inicio + k][navio2_coluna_inicio] = 3;
+    }
+
+
+
+    // Navio 3: 3 posições na diagonal principal começando em A, 0
+    for (int l = 0; l < 3; l++) {
+        tabuleiro[l][l] = 3; 
+    }
+
+    // Navio 4: 3 posições na diagonal secundária começando em A, 9
+    for (int m = 0; m < 3; m++) {
+        tabuleiro[m][9 - m] = 3;
     }
 
 
